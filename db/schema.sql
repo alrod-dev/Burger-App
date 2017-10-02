d897e3c8914ad84b9ab04dbb334caca18d1faa28
Alfredo Rodriguez <alrod909@gmail.com>
2017-10-02 13:05:24 -0600

    Most Files

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+
+CREATE DATABASE medCheck_db;
+USE medCheck_db;
+
+CREATE TABLE meds
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	description varchar(255) NOT NULL,
+	dosage INTEGER,
+	frequency INTEGER,
+	doctor_Name varchar(255) NOT NULL,
+	phoneNumber varchar(255) NOT NULL,
+	PRIMARY KEY (id)
+);
